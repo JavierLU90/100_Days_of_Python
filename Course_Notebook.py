65f2fb66185712ae43e6788a133f56b4a935282a
JavierLU90 <jlomelinu@gmail.com>
2025-05-29 18:28:04 -0600

    Finish Day 1

---
+++ Course_Notebook.py
@@ -0,0 +1,13 @@
+# 100 DAYS OF CODE - PYTHON
+# COURSE NOTEBOOK
+
+# Printing
+print("Hello World!")
+print("Hello\nHello\nHello") # \n adds new line
+print("Hello" + " " + "Javier") # + concatenates the strings
+
+# input() Function
+variable = input("A prompt for the user") # stores the user input in a variable
+
+# len() function
+len(variable) # returns the length of a string
