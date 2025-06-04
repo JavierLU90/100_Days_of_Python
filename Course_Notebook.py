6ffa429606ff4a6e5da624e2f6c1c8b305b943ac
JavierLU90 <jlomelinu@gmail.com>
2025-06-04 13:04:47 -0600

    Day 2

--- Course_Notebook.py
+++ Course_Notebook.py
@@ -11,3 +11,44 @@ variable = input("A prompt for the user") # stores the user input in a variable
 
 # len() function
 len(variable) # returns the length of a string
+
+# Specific characters in strings
+my_str = "Hello"
+my_str[0] # "H"
+my_str[1] # "e"
+my_str[-1] # Last character ("o")
+
+# Data types:
+my_str = "Javier" # string = text characters
+my_int = 123 # int = Whole numbers
+my_float = 3.1416 # float = Numbers with decimals
+my_bool = True # bool = True or false (0 or 1)
+
+# Check data type:
+type(variable)
+
+# Convert data types:
+int() # converts into int
+float() # converts into float
+str() # converts into string
+# Not everything can be converted.
+
+# Basic Operators:
+# + : add, - : subtract, * : multiply, / : divide
+# // : divide whole numbers, ** : exponents
+# PEMDAS -> order of operations
+
+# Flooring and Rounding
+int() # also floors the number (removes all digits after the decimal)
+round(my_float) # rounds to the nearest whole number
+round(my_float, 2) # rounds to 2 decimal places
+
+# Assignemnt Operators
+# += , -= , *= , /=
+# Add, subtract, multiply or divide the right of the equation to the left side 
+# and assign the new value to the variable
+
+# f-Strings:
+age = 12
+print(f"I am {age} years old")
+# modifies print statements to work with variables.
