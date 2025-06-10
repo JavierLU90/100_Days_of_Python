09f01c72c1da59dea23b7560bc243e1f5fa28767
JavierLU90 <jlomelinu@gmail.com>
2025-06-10 17:02:04 -0600

    Day 9

--- Course_Notebook.py
+++ Course_Notebook.py
@@ -1,21 +1,21 @@
 # 100 DAYS OF CODE - PYTHON
 # COURSE NOTEBOOK
 
-# Printing
+# Printing:
 print("Hello World!")
 print("Hello\nHello\nHello") # \n adds new line
 print("Hello" + " " + "Javier") # + concatenates the strings
 
 
-# input() Function
+# input() Function:
 variable = input("A prompt for the user") # stores the user input in a variable
 
 
-# len() function
+# len() function:
 len(variable) # returns the length of a string
 
 
-# Specific characters in strings
+# Specific characters in strings:
 my_str = "Hello"
 my_str[0] # "H"
 my_str[1] # "e"
@@ -43,13 +43,13 @@ str() # converts into string
 # PEMDAS -> order of operations
 
 
-# Flooring and Rounding
+# Flooring and Rounding:
 int() # also floors the number (removes all digits after the decimal)
 round(my_float) # rounds to the nearest whole number
 round(my_float, 2) # rounds to 2 decimal places
 
 
-# Assignemnt Operators
+# Assignemnt Operators:
 # += , -= , *= , /=
 # Add, subtract, multiply or divide the right of the equation to the left side 
 # and assign the new value to the variable
@@ -80,14 +80,14 @@ remainder = 5 % 2 # remainder = 1
 # and , or , not
 
 
-# Random Module
+# Random Module:
 import random # imports module
 random_int = random.randint(1, 10) # generates random int between 1 and 10 (inclusive)
 random_float = random.random() # generates random float 0 <= n < 1
 random_float = random.uniform(1, 10) # generates random float from range 1 <= n <= 10
 
 
-# Lists
+# Lists:
 fruits = ["Cherry", "Apple", "Pear"]
 fruits[0] # "Cherry"
 fruits[-1] # "Pear" - Last item in list
@@ -96,27 +96,27 @@ fruits.append("Cherry") # Adds single item at the end of the list
 random_item = random.choice(fruits) # selects a random item from a list
 
 
-# For Loops
+# For Loops:
 for fruit in fruits: # loops through every element in list
     print(fruit) # prints every item in fruits list
 for number in range(1, 100): # loops from 1 (inc) to 100 (not inc)
     print(number)
 
 
-# While Loops
+# While Loops:
 something_is_true = True
 while something_is_true:
     print("This is true")
     something_is_true = False
 
 
-# Functions
+# Functions:
 def my_function(): # create a function
     print("Hello")
 my_function() # call the function
 
 
-# Imports
+# Imports:
 # You can import from other modules (for example: random) or from other files in your project.
 # You can import the whole module/file using "import <name_of_module>.""
 # Or you can import pieces from the module/file.
@@ -128,3 +128,16 @@ def new_function(my_string): # create function, require an input
     print(my_str)
 new_function("Hello") # call function by passing in an input
 # Parameter = my_string, argument = "Hello"
+
+
+# Dictionaries:
+my_dictionary = {"Key 1": "Value 1", 
+                 "Key 2": "Value 2",
+                 "Key n": "Value n",} # Creating a dictionary.
+print(my_dictionary["Key 1"]) # prints the value stored in "Key 1".
+my_dictionary["Key x"] = "Value x" # Add new key/value to a dictionary.
+my_dictionary = {} # creates an empty dictionary, or wipes the data of an existing one.
+for key in my_dictionary: # Loop through a dictionary.
+    print(key) # Print all the keys.
+    print(my_dictionary[key]) # Print all the values.
+# The values of a dictionary can be lists or even dictionaries.
